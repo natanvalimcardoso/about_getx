cf1cfc4edbb8a14f7a62713ad4e7f6628c9285a2
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-11-10 09:19:54 -0300

    ready

---
+++ lib/getx_rotas/rotas_nomeadas/pages/arguments/envio_arguments_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EnvioArgumentsPage extends StatelessWidget {
+
+  const EnvioArgumentsPage({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Enviando Arguments'),),
+           body: Container(),
+       );
+  }
+}

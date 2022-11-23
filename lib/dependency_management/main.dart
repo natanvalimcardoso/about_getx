5dab03fe080d629503f2ba8541056048dafcfb9b
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-11-23 20:04:41 -0300

    next

---
+++ lib/dependency_management/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Main extends StatelessWidget {
+
+  const Main({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return const GetMaterialApp(
+
+       );
+  }
+}
---
+++ lib/dependency_management/pages/basico/basico_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BasicoHomePage extends StatelessWidget {
+
+  const BasicoHomePage({ Key? key }) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
